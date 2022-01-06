3f4d688517fc471a804d9e6fa839de071127a6c6
Hassan Mahmoud Hassan <55447090+hassan510-cmd@users.noreply.github.com>
2022-01-06 13:58:59 +0200

    fix conflict until wizard

--- hospital/models/__init__.py
+++ hospital/models/__init__.py
@@ -4,9 +4,5 @@ from . import models
 from . import patient
 from . import parent
 from . import medicine
-<<<<<<< HEAD
 from . import medicineSaleOrder
 from . import appointment
-=======
-from . import medicineSaleOrder
->>>>>>> c0b7a710f6d45271931955afc06d35a481300320
